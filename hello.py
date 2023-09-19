a52c0cee606650101383cde98770d917d8a477f5
Ardavan Alaei Fard <ardavanalaei@gmail.com>
2023-09-19 15:21:32 -0400

    Added a navigate bar to the top

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
-
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!'.format(name)
+    return render_template('user.html', name='Ardavan')
