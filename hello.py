cac2e14ae3ab383df4eaf69333cf67d6b1b74407
Ardavan Alaei Fard <ardavanalaei@gmail.com>
2023-09-22 19:45:08 -0400

    Added field for name

--- hello.py
+++ hello.py
@@ -1,16 +1,35 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from datetime import datetime
 
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'abi#83jvmf93'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+"""
 @app.route('/')
 def index():
     return render_template('user.html', name='Ardavan',
                            current_time=datetime.utcnow())
+"""
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', name=name, form=form)
 
 if __name__ == '__main__':
     app.run()
