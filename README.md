6f274a1cd15a62c2d34bb13116b74b6dd493afa9
Ardavan Alaei Fard <ardavanalaei@gmail.com>
2023-09-22 18:25:22 -0400

    Added local time and screenshot

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 this repo is a clone of https://github.com/miguelgrinberg/flasky
 ![Alt text](images/activity_1.png)
 ![Alt text](images/activity_2.png)
+![Alt text](images/activity_3.png)
--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('user.html', name='Ardavan')
+    return render_template('user.html', name='Ardavan',
+                           current_time=datetime.utcnow())
+
+if __name__ == '__main__':
+    app.run()
