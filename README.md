80b73c59c56213676ae2678dfa2929e85a3b8b2e
Ardavan Alaei Fard <ardavanalaei@gmail.com>
2023-09-23 00:12:33 -0400

    Added function of adding UofT email and screenshots for activity 4

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ this repo is a clone of https://github.com/miguelgrinberg/flasky
 ![Alt text](images/activity_1.png)
 ![Alt text](images/activity_2.png)
 ![Alt text](images/activity_3.png)
+![Alt text](images/activity_4_1.png)
+![Alt text](images/activity_4_2.png)
+![Alt text](images/activity_4_3.png)
+![Alt text](images/activity_4_4.png)
--- hello.py
+++ hello.py
@@ -2,19 +2,20 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email
 from datetime import datetime
 
 num = 1
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abi#83jvmf93'
 app.config['SESSION_PERMANENT'] = True
-app.config['PERMANENT_SESSION_LIFETIME'] = 60
+app.config['PERMANENT_SESSION_LIFETIME'] = 3600     # reset session every hour
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
@@ -28,15 +29,18 @@ def index():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+    session['submitted_email'] = form.email.data
     if form.validate_on_submit():
         old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            print(old_name)
-            print(form.name.data)
+        old_email = session.get('email')
+        if old_name is not None and old_name != form.name.data:     # check if the name has changed
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:     # check if the email has changed
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 if __name__ == '__main__':
     app.run()
