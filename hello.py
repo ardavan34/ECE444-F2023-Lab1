0d2f5e8d46bd714c5850d3bb302d7f13386a511b
Ardavan Alaei Fard <ardavanalaei@gmail.com>
2023-09-22 22:00:43 -0400

    Added change of name message and finished textbook example 4-7

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -6,12 +6,15 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from datetime import datetime
 
+num = 1
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abi#83jvmf93'
+app.config['SESSION_PERMANENT'] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = 60
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
@@ -24,12 +27,16 @@ def index():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', name=name, form=form)
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            print(old_name)
+            print(form.name.data)
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 if __name__ == '__main__':
     app.run()
